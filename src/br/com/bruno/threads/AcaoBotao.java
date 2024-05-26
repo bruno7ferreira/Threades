c56a1130a208a4e15fcc510d67df3dc65ae4338a
Bruno Rogério <b.rogerio2007@gmail.com>
2024-05-26 19:34:27 -0400

    Exercicio

--- src/br/com/bruno/threads/AcaoBotao.java
+++ src/br/com/bruno/threads/AcaoBotao.java
@@ -19,27 +19,19 @@ public class AcaoBotao implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                long valor1 = Long.parseLong(primeiro.getText());
-                long valor2 = Long.parseLong(segundo.getText());
-                BigInteger calculo = new BigInteger("0");
+        Thread thread = new Thread(() -> {
+            long valor1 = Long.parseLong(primeiro.getText());
+            long valor2 = Long.parseLong(segundo.getText());
+            BigInteger calculo = new BigInteger("0");
 
-                for (int i = 0; i < valor1; i++) {
-                    for (int j = 0; j < valor2; j++) {
-                        calculo = calculo.add(BigInteger.ONE);
-                    }
+            for (int i = 0; i < valor1; i++) {
+                for (int j = 0; j < valor2; j++) {
+                    calculo = calculo.add(BigInteger.ONE);
                 }
-
-                final BigInteger finalCalculo = calculo;
-                SwingUtilities.invokeLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        resultado.setText(finalCalculo.toString());
-                    }
-                });
             }
+
+            final BigInteger finalCalculo = calculo;
+            SwingUtilities.invokeLater(() -> resultado.setText(finalCalculo.toString()));
         });
         thread.start();
     }
